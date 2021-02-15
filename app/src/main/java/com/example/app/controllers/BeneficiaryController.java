8038e9a4c5f17f466e6c767f9f6a15b33e6365ba
Matheus Augusto <matheus.augusto.jp@gmail.com>
2021-02-14 21:27:55 -0300

    Adiciona configuracao de crossOrigin da api

--- app/src/main/java/com/example/app/controllers/BeneficiaryController.java
+++ app/src/main/java/com/example/app/controllers/BeneficiaryController.java
@@ -1,12 +1,10 @@
 package com.example.app.controllers;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.app.repositories.BeneficiaryRepository;
-
-import java.util.List;
-
 import com.example.app.entities.Beneficiary;
 
 @RestController
@@ -20,6 +18,7 @@ public class BeneficiaryController {
 
     @GetMapping("api/beneficiaries")
     public List<Beneficiary> get(@RequestParam(required = false) Long id) {
+
         return id != null
             ?  this.beneficiaryRepository.getBeneficiary(id)
             :  this.beneficiaryRepository.getAllBeneficiaries();
